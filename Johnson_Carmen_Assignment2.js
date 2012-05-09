2bf9cd99c0bf95ce84aaf61e2f5466c4ae229287
Carmen Johnson <blackpalmmobileapplications@gmail.com>
2012-05-09 15:42:38 -0700

    Added first files to the repo.

---
+++ Johnson_Carmen_Assignment2.js
@@ -0,0 +1,23 @@
+alert("JavaScript works!");
+
+var des = "designer";
+var prog = "programming team";
+var ht = "hire";
+var hireCompanies = ["AppStars", "CodeBand"]
+var companyA = "AppStart";
+var timeA = 3
+var companyB = "CodeBang";
+var timeB = 4
+expA = ["graphics","designing", "animation", "designers"];
+expB = ["coding", "programming", "Java"]
+var bestDesigner = expA = expA.length;
+var pickCompany = function (whatCompany) {
+       var hireCompany = hireCompanies [whatCompany],
+//if companyA < companyB then the game will be developed sooner.
+   // If timeA < timeB
+//if companB > companyA then the game will be developed later.
+//if A === B then the game will have the best programmers and designers from both companies.
+    // }else{
+      //  console.log(“I will have to design the game and” + ht + companyB  to do the + expB[1] “.”)
+console.log( + hireCompanies[0] + “does a faster job, but ” + hireCompanies[1] + “ has better ” + expA[2] “.”);
+
